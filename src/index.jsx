ecf9833e73865be1865d0b17c41f1b034c689bf2
tai-fujio <taisaburo.fujio@gmail.com>
2020-03-02 18:16:48 +0900

    SearchComponent作成

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App2 from './components/App2';
+import App3 from './components/App3';
 
-ReactDOM.render(<App2 />, document.querySelector('.container'));
+ReactDOM.render(<App3 />, document.querySelector('.container'));
