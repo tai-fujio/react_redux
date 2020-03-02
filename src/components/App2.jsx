a3cfd94d99c43db87e89688e6fac02487e70ce35
tai-fujio <taisaburo.fujio@gmail.com>
2020-03-02 17:33:00 +0900

    2way_bindingテスト

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -2,9 +2,39 @@ import React , {Component} from 'react';
 import Greeting from './greeting';
 
 class App2 extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'Ana',
+    };
+  };
+
+  handleMouseOver(){
+    this.setState({name:'Mike'})
+  };
+
+  handleMouseOut(){
+    this.setState({name:'Pan'})
+  };
+
+  handleNameChange(name){
+    // console.log(e.target.value);
+    this.setState({name: name});
+  };
+
+
+
   render(){
     return(
-      <div>
+      <div onMouseOver={() => this.handleMouseOver()} onMouseOut={() => this.handleMouseOut()}
+      >
+      <input 
+        type="text" 
+        value={this.state.name}
+        onChange={e => this.handleNameChange(e.target.value)}
+        />
+        <button onClick={() => this.handleNameChange('Bob')}>Set Name Bob</button>
+        <Greeting name={this.state.name} />
         <Greeting name="Bob" />
         <Greeting name="Sara" />
       </div>
